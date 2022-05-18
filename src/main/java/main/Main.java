1e7299c8917150e284c2b7df39b57417346b4ae5
Junior Egger <junior.egger@gmail.com>
2022-05-18 17:55:57 -0300

    CRIANDO CLASSE POSITION

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,11 @@
 package main;
 
+import boardgame.position.Position;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        Position position = new Position(3, 5);
+        System.out.println(position);
     }
 }
