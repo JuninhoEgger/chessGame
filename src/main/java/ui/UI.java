433a984213d74c7b1182af748d3a78f8ffdc3f99
Junior Egger <junior.egger@gmail.com>
2022-05-18 19:42:40 -0300

    Little improvement in board printing

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -2,6 +2,7 @@ package ui;
 
 import chess.ChessPiece;
 
+import static chess.colorenum.Color.WHITE;
 import static java.lang.System.out;
 
 public class UI {
@@ -9,6 +10,27 @@ public class UI {
     private UI() {
         //CONSTRUTOR VAZIO APONTADO PELO SONAR
     }
+
+    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
@@ -25,7 +47,11 @@ public class UI {
         if (piece == null) {
             out.print("-");
         } else {
-            out.print(piece);
+            if (piece.getColor() == WHITE) {
+                out.print(ANSI_WHITE + piece + ANSI_RESET);
+            } else {
+                out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         out.print(" ");
     }
